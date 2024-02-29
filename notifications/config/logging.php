324663ccb7f8ac483315693c18ce4f53391ee187
justin kekeocha <justinratzinger@gmail.com>
2024-02-29 16:04:04 +0100

    feat: create demo log channel

--- notifications/config/logging.php
+++ notifications/config/logging.php
@@ -89,7 +89,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
@@ -125,6 +125,12 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+
+        'demo' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/demo.log'),
+            'level' => 'debug',
         ],
     ],
 
